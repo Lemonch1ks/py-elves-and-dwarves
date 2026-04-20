1a9082ed6818e0f1840174260567ac4e3cc2dc17
Oleksandr Dvirko <dvalex02@gmail.com>
2026-04-20 12:55:15 +0300

    adding structure

--- app/main.py
+++ app/main.py
@@ -1,1 +1,63 @@
-# write your code here
+from app.players.player import Player
+
+
+class Dwarf(Player):
+    def __init__(self, nickname: str, favourite_dish: str) -> None:
+        self._favourite_dish = favourite_dish
+        self._nickname = nickname
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self._nickname} is eating {self._favourite_dish}")
+
+
+
+
+
+class Druid(Elf):
+    def __init__(self, nickname: str, musical_instrument: str, favourite_spell: str) -> None:
+        super().__init__(nickname, musical_instrument)
+        self._favourite_spell = favourite_spell
+
+    def player_info(self) -> str:
+        return f"Druid {self._nickname}. {self._nickname} has a favourite spell: {self._favourite_spell}"
+
+    def get_rating(self) -> int:
+        return len(self._favourite_spell)
+
+
+class DwarfWarrior(Dwarf):
+    def __init__(self, nickname: str, favourite_dish: str, hummer_level: int) -> None:
+        super().__init__(nickname, favourite_dish)
+        self._hummer_level = hummer_level
+
+    def player_info(self) -> str:
+        return f"Dwarf warrior {self._nickname}. {self._nickname} has a hummer of the {self._hummer_level} level"
+
+    def get_rating(self) -> int:
+        return self._hummer_level + 4
+
+
+class DwarfBlacksmith(Dwarf):
+    def __init__(self, nickname: str, favourite_dish: str, skill_level: int) -> None:
+        super().__init__(nickname, favourite_dish)
+        self._skill_level = skill_level
+
+    def player_info(self) -> str:
+        return f"Dwarf blacksmith {self._nickname} with skill of the {self._skill_level} level"
+
+    def get_rating(self) -> int:
+        return self._skill_level
+
+
+def calculate_team_total_rating(players: list[Player]) -> int:
+    return sum(player.get_rating() for player in players)
+
+
+def elves_concert(list_of_elves: list[Elf]) -> None:
+    for elf in list_of_elves:
+        elf.play_elf_song()
+
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
+
